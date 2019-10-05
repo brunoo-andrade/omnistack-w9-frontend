e9646db35871793d04c91ff36e4ff008102e37db
Bruno Andrade <bruno.andrade@fastsolucoes.com.br>
2019-10-05 20:34:27 -0300

    Route structure creation and componentization

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
+
 import './App.css';
 
 import logo from './assets/logo.svg';
+import Routes from './routes';
 
 function App() {
-    const [email, setEmail] = useState('');
-
-    async function handlerSubmit(event) {
-        event.preventDefault();
-
-        const response = await api.post('/sessions', { email });
-        const { _id } = response.data;
-
-        localStorage.setItem('aircnc_user', _id);
-    }
-
     return (
         <div className="container">
             <img src={ logo } alt="Logo AirCnc" ></img>
 
             <div className="content">
-                <p>
-                    Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-                </p>
-
-                <form onSubmit={handlerSubmit}>
-                    <label htmlFor="email">E-MAIL *</label>
-                    <input
-                        id="email"
-                        type="email"
-                        placeholder="Seu melhor e-mail"
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
-                    />
-                    <button className="btn-primary" type="submit"> Entrar </button>
-                </form>
+                <Routes />
             </div>
         </div>
     );
