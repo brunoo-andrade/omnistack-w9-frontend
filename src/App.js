67c257a25a77e4d089dc649acb453bd3d35e9ef0
Bruno Andrade <bruno.andrade@fastsolucoes.com.br>
2019-10-05 20:02:42 -0300

    create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+    const [email, setEmail] = useState('');
+
+    async function handlerSubmit(event) {
+        event.preventDefault();
+
+        const response = await api.post('/sessions', { email });
+        const { _id } = response.data;
+
+        localStorage.setItem('aircnc_user', _id);
+    }
+
     return (
         <div className="container">
             <img src={ logo } alt="Logo AirCnc" ></img>
@@ -13,12 +25,14 @@ function App() {
                     Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
                 </p>
 
-                <form>
+                <form onSubmit={handlerSubmit}>
                     <label htmlFor="email">E-MAIL *</label>
                     <input
                         id="email"
                         type="email"
                         placeholder="Seu melhor e-mail"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
                     />
                     <button className="btn-primary" type="submit"> Entrar </button>
                 </form>
